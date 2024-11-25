d339c79efcfc1eb6557d8dc784babd77a193041a
Ryuyena0305 <Ryuyena0305@gmail.com>
2024-11-25 12:27:49 +0900

    day11 3교시

--- day11/1_조건문문제.js
+++ day11/1_조건문문제.js
@@ -41,25 +41,49 @@
 
 
 // 4. 3개의 정수를 각 입력받아 오름차순 또는 내림차순 하여 정렬해서 출력하시오.
+
+/*
+ A   B   C
+        7   5   10 --> 오름차순 : 5 7 10  , 내림차순 : 10 7 5
+        [ 1.정렬/비교 ]                                                    A      B       C
+            ( 오름차순 )                                                   10     7       9
+            1. A와 B 비교 해서 B가 더 작으면 B를 A와B 값 교체 A<--->B        7      10      9  교환O
+                if( a < b ){ } 
+            2. A와 C 비교 해서 C가 더 작으면 C를 A와C 값 교체 A<--->C        7      10      9  교환X
+                if( a < c ){  }
+            3. B와 C 비교 해서 C가 더 작으면 C를 B와C 값 교제 B<--->C        7      9       10 교환O
+                if( b < c ) { }
+            ( 내림차순 ) : 부등호 반대 , 더 크면 
+        [ 2.변수 값 교환/swap ]
+            - 한번에 2개 연산을 동시에 처리할수 없다. 무조건 연산은 1개씩 처리한다.
+            - 변수는 *무조건* 데이터/자료 1개만 저장 ....
+            - 주의할점  A = B ; A변수에 B 값 를 대입하면 기존의 A값은 사라진다.
+                        A = B; B = A; // - 컴퓨터 순차처리 이므로 A값은 이미 사라졌다.  
+            - swap 방법 
+                임시변수 temp 활용
+                let temp = A; A = B; B = temp;
+*/
 // let int1 =Number( prompt('정수1를 입력하세요.'));
 // let int2 =Number( prompt('정수2를 입력하세요.'));
 // let int3 =Number( prompt('정수3를 입력하세요.'));
-// let max, min, mid ;
+
 // if(int1<int2){
-//     max = int2;
-//     min =int1
+//     let temp =int1;
+//     int1 =int2;
+//     int2 =temp;
 // }
-// else if(max<int3){
-//     max = int3;
-//     mid=int2;
-//     min=int1
+// if(int1<int3){
+//     let temp =int1;
+//     int1 =int3;
+//     int3 =temp;
 // }
-// else if(max<int1){
-//     max=int1;
-//     mid=int2;
-//     min=int3
+// if(int2<int3){
+//     let temp = int2;
+//     int2=int3;
+//     int3=temp;
 // }
-// else max;
+// console.log(`결과 : ${int1} ${int2} ${int3}`);
+
 
 
 
@@ -92,21 +116,22 @@
 // 7. 과일 배열 선언하고 let fruitList = [ '바나나' , '사과' ]
 // 하나의 과일명을 입력받아 fruitList 에 존재하는 과일이면 '이미 존재하는 과일입니다' 출력
 // 아니면 배열에 입력받은 과일명 저장 하고 현재 배열을 출력하시오.
-let fruitList = [ 'banana' , 'apple' ];
-let plus_fruit = prompt('과일을 입력하세요.');
+let fruitList = [ '바나나' , '사과' ];
+let 과일명 = prompt('과일을 입력하세요.');
+let isfruit = fruitList.indexOf(과일명);
+//.indexOf('찾을값') : 지정한 찾을값이 배열내 존재하면 찾은 인덱스 봔환 없으면 -1 반환 함수
+//.push('새로운값') : 지정한 새로운값을 배열내 마지막 인덱스 요소의 추가 함수
 
-if(plus_fruit == fruitList[0]){
-    console.log('이미 존재하는 과일입니다.');
-}
-else if(plus_fruit == fruitList[1]){
-    console.log('이미 존재하는 과일입니다.');
-}
-else{
-    fruitList.push(plus_fruit);
+if(isfruit==-1){
+    fruitList.push(과일명);
     console.log(fruitList);
+}
+else {
+    console.log('이미 존재하는 과일입니다.');
 }
 
 // 8. 주민등록번호를 입력받아서 성별이 남자이면 색상변수에 'blue' 을 여자이면 'red' 을 대입 하고 색상변수 출력하시오.
+
 
 // 9. 가위바위보 게임
 // 조건1 : 가위 0 , 바위 1 , 보 2 입니다.
